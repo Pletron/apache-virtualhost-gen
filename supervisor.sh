5786ad6bb7d85c90dea297ece8ffdf23d6946b70
Philip Masek <philip.masek@gmail.com>
2016-05-15 19:42:31 +0200

    Update supervisor.sh

--- supervisor.sh
+++ supervisor.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+echo "[Docker supervisor] Started"
+echo "[Docker supervisor] Sleeping"
+sleep 20
+echo "[Docker supervisor] Configuring virtual host"
 chmod +x /lampstack-docker-config/generate-virtualhost.sh && /lampstack-docker-config/generate-virtualhost.sh
