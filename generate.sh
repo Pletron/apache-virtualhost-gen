e9d9591b5af84c5f3cac3eb53e08ef0f12191ff8
Philip Masek <philip.masek@gmail.com>
2016-05-15 18:27:23 +0200

    Create generate.sh

---
+++ generate.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+hostDir=/var/www
+
+fullurl="$(head -n 1 Dockerfile | awk '{print $2}')"
+subdomain=$(echo $fullurl | cut -f 1 -d ".")
+IFS='.' read -r id sitename <<< "$fullurl"
+
+echo $'<VirtualHost *:80>
+    ServerName '$sitename'
+    ServerAlias '$subdomain'.'$sitename'
+    DocumentRoot '$hostDir'/'$fullurl'
+    CustomLog '$hostDir'/'$fullurl'/access.log combined
+    ErrorLog '$hostDir'/'$fullurl'/error.log
+    <Directory "'$hostDir'/site">
+      Options FollowSymLinks Includes MultiViews
+      AllowOverride All
+      Order allow,deny
+      Allow from all
+      Require all granted
+    </Directory>
+</VirtualHost>' > /etc/apache2/sites-available/$fullurl
+
+ln -s /etc/apache2/sites-available/$fullurl /etc/apache2/sites-enabled/$fullurl
+apache2ctl -k graceful
